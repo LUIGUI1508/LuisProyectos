671d25bff13262770e30c7abc72a6c63e28c19fd
invitad <invitad@LUIGUI.mshome.net>
2023-01-13 18:19:26 -0500

    Segunda version del examen

---
+++ Examen Primer Parcial/src/ec/edu/intsuperior/modelo/Ejecutivo.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.modelo;
+
+/**
+ *
+ * @author invitad
+ */
+public class Ejecutivo extends Empleado {
+    private String categoria;
+
+    public Ejecutivo() {
+    }
+
+    public Ejecutivo(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    
+    
+    
+}
